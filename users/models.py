8655f3a2e962411e778d93d301693c5fab3151dd
Satyamkumarai <satyamkumarai@gmail.com>
2020-06-21 21:00:47 +0530

    args and kwargs to save method

--- users/models.py
+++ users/models.py
@@ -13,8 +13,8 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     #overriding the save method to resize the images while being saved.
-    def save(self):
-        super().save()
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
         img = Image.open(self.image.path)
         if img.height>300 or img.width>300:
             outputSize = (300,300)
